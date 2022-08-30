7f25e2df02089d6eddb52327d0901657313cb38c
yeeyson.duarte <yeeyson.duarte@DEVSCOPE335>
2022-08-30 17:25:00 +0100

    Add project files.

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();  // Redirects HTTP requests to HTTPS
+app.UseStaticFiles(); // Enables static files, such as HTML, CSS, images, and JavaScript to be served.
+
+app.UseRouting(); //  Adds route matching to the middleware pipeline
+
+app.UseAuthorization(); // Authorizes a user to access secure resources
+
+app.MapRazorPages();  // Configures endpoint routing for Razor Pages
+
+app.Run();  //  Runs the app.
